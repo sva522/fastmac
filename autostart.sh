16d529f259e55e9eaf8bfd53bb920ce2fcc39403
admin <no@mail.org>
2020-10-20 22:04:45 +0200

    OK

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,6 @@
 sudo apt-get update -y
 sudo apt-get install -y make gcc neofetch powerline
 sudo curl https://getmic.ro | bash
-sudo micro -plugin install gotham-colors
 
 # Always start vm
 exit 0
