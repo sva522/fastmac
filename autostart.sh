f10874b494441bb1bee633a6f638f1835dc0abdd
sva522 <xavier.sudres@gmail.com>
2021-12-16 19:40:14 +0100

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 # Update distro
-sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt update
+sudo apt -y upgrade
+
+# Remove blocked packages
+sudo apt remove mysql*
+
+sudo sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades
+
+# Update to last version (no lts)
+do-release-upgrade -f DistUpgradeViewNonInteractive
+
 # Install base packages
-sudo apt-get install -y cmake make gcc neofetch fish nmap micro
+sudo apt install -y cmake make gcc neofetch fish nmap micro
 
 # Add settings
 echo neofetch >> ~/.bashrc
 sudo sed -i 's+/home/runner:/bin/bash+/home/runner:/usr/bin/fish+g' /etc/passwd
-cat nanorc > /etc/nanorc
+sudo cat nanorc > /etc/nanorc
 
 # Always start vm
 exit 0
