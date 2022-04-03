6dc02b066cb8a9e2345861ae3a5737bfa14813e5
sva522 <xavier.sudres@gmail.com>
2022-04-03 22:08:13 +0200

    OK

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
 # Update distro
 sudo apt update
 sudo apt -y upgrade
 
-## Update to last version (no lts)
-### Too long => disabled
+## Update to last version (no lts) # Too long => disabled
 ## Enable normal release
 # sudo sed -i 's/Prompt=lts/Prompt=normal/g' /etc/update-manager/release-upgrades
 ## Remove blocked packages
@@ -14,12 +15,8 @@ sudo apt -y upgrade
 # do-release-upgrade -f DistUpgradeViewNonInteractive
 
 # Install base packages
-sudo apt install -y cmake make gcc neofetch fish nmap micro
-
-# Add settings
-echo neofetch >> ~/.bashrc
-sudo sed -i 's+/home/runner:/bin/bash+/home/runner:/usr/bin/fish+g' /etc/passwd
-sudo cp -f nanorc /etc/nanorc
+sudo apt install -y ansible
+ansible-playbook playbook.yaml
 
 # Always start vm
 exit 0
